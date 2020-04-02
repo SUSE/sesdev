SUSE/sesdev

commit a52f94eb3faa24fe1505cf8f6ded620f743d8f52

seslib: raise exception more, or less, than one master/bootstrap role
A check that quickly bails out if someone omits the "master" role was
implemented by ddd4d91ca08f3e4ea993d9cba9431373c62fed8b but that commit
is being reverted and replaced by this solution which implements the
check in a less intrusive way, later in the "sesdev create" process.

Signed-off-by: Nathan Cutler <ncutler@suse.com>

seslib/__init__.py
@@ -19,7 +19,7 @@ from .exceptions import DeploymentDoesNotExists, VersionOSNotSupported, SettingT
                         ServicePortForwardingNotSupported, DeploymentAlreadyExists, \
                         ServiceNotFound, ExclusiveRoles, RoleNotSupported, CmdException, \
                         VagrantSshConfigNoHostName, ScpInvalidSourceOrDestination, \
-                        TooManyMasters, SettingNotKnown, SupportconfigOnlyOnSLE
+                        UniqueRoleViolation, SettingNotKnown, SupportconfigOnlyOnSLE
 
 
 JINJA_ENV = Environment(loader=PackageLoader('seslib', 'templates'), trim_blocks=True)
@@ -751,8 +751,10 @@ class NodeManager:
 
 
 class Deployment():
-    def __init__(self, dep_id, settings):
+
+    def __init__(self, dep_id, settings, load=False):
         self.dep_id = dep_id
+        self.from_load = load
         self.settings = settings
         self.nodes = {}
         self.node_counts = {
@@ -1012,8 +1014,11 @@ class Deployment():
 
             self.nodes[node.name] = node
 
-        if self.node_counts['master'] > 1:
-            raise TooManyMasters(self.node_counts['master'])
+        if not self.from_load:
+            if self.node_counts['master'] != 1:
+                raise UniqueRoleViolation('master', self.node_counts['master'])
+            if self.node_counts['bootstrap'] != 1:
+                raise UniqueRoleViolation('bootstrap', self.node_counts['bootstrap'])
 
         if self.master and self.suma:
             raise ExclusiveRoles('master', 'suma')
@@ -1578,7 +1583,7 @@ class Deployment():
         with open(metadata_file, 'r') as file:
             metadata = json.load(file)
 
-        dep = cls(metadata['id'], Settings(strict=False, **metadata['settings']))
+        dep = cls(metadata['id'], Settings(strict=False, **metadata['settings']), load=True)
         if load_status:
             dep.load_status()
         return dep

sesdev/__init__.py
@@ -4,7 +4,7 @@ import sys
 import click
 import pkg_resources
 import seslib
-from seslib.exceptions import SesDevException, NoMasterRoleInRoles
+from seslib.exceptions import SesDevException
 
 
 logger = logging.getLogger(__name__)
@@ -124,13 +124,9 @@ def common_create_options(func):
 
 
 def _parse_roles(roles):
-    log_msg = "Parsing role string ->{}<-".format(roles)
     roles = [r.strip() for r in roles.split(",")]
-    logger.info("_parse_roles: " + log_msg)
-    click.echo(log_msg)
     _roles = []
     _node = None
-    master_present = False
     for role in roles:
         role = role.strip()
         if role.startswith('['):
@@ -151,12 +147,6 @@ def _parse_roles(roles):
         else:
             role = role.strip()
             _node.append(role)
-        if role == 'master':
-            master_present = True
-    if master_present:
-        logger.debug("_parse_roles: master role detected - good!")
-    else:
-        raise NoMasterRoleInRoles()
     return _roles
 
 

seslib/exceptions.py
@@ -90,10 +90,12 @@ class RoleNotSupported(SesDevException):
             "Role '{}' is not supported in version '{}'".format(role, version))
 
 
-class TooManyMasters(SesDevException):
-    def __init__(self, number):
-        super(TooManyMasters, self).__init__(
-            "There can only be one 'master' role (you gave {})'".format(number))
+class UniqueRoleViolation(SesDevException):
+    def __init__(self, role, number):
+        super(UniqueRoleViolation, self).__init__(
+            "There must be one, and only one, '{role}' role "
+            "(you gave {number} '{role}' roles)".format(role=role, number=number)
+            )
 
 
 class VagrantSshConfigNoHostName(SesDevException):
@@ -120,12 +122,4 @@ class SupportconfigOnlyOnSLE(SesDevException):
         super(SupportconfigOnlyOnSLE, self).__init__(
             "sesdev supportconfig depends on the 'supportconfig' RPM, which is "
             "available only on SUSE Linux Enterprise"
-            )
-
-
-class NoMasterRoleInRoles(SesDevException):
-    def __init__(self):
-        super(NoMasterRoleInRoles, self).__init__(
-            "No 'master' role specified - please check the roles and make sure one, "
-            "and only one, node has the 'master' role"
             )

commit 5100786d82de471146223182a9d30ded9c0491d7

seslib: check bootstrap role only when it is relevant
Fixes: https://github.com/SUSE/sesdev/issues/212
Fixes: a52f94eb3faa24fe1505cf8f6ded620f743d8f52
Signed-off-by: Nathan Cutler <ncutler@suse.com>

seslib/__init__.py
@@ -1017,7 +1017,7 @@ class Deployment():
         if not self.from_load:
             if self.node_counts['master'] != 1:
                 raise UniqueRoleViolation('master', self.node_counts['master'])
-            if self.node_counts['bootstrap'] != 1:
+            if self.settings.version in ('ses7', 'octopus') and self.node_counts['bootstrap'] != 1:
                 raise UniqueRoleViolation('bootstrap', self.node_counts['bootstrap'])
 
         if self.master and self.suma: