SUSE/sesdev

commit 231e50d41c62a8c62485fd59b3ca79503cd264c2

ceph_salt_deployment: make it easier to install ceph-salt from source
Before, one had to provide both "--ceph-salt-repo" and
"--ceph-salt-branch" in order for sesdev to install ceph-salt from
source.

With this patch, only one or the other is required.

When only one of these two options is given, the other will default to
a sensible value. The defaults are hard-coded to:

--ceph-salt-repo https://github.com/ceph/ceph-salt
--ceph-salt-branch master

Signed-off-by: Nathan Cutler <ncutler@suse.com>

seslib/__init__.py
@@ -30,6 +30,8 @@ logger = logging.getLogger(__name__)
 
 class GlobalSettings():
     A_WORKING_DIR = os.path.join(Path.home(), '.sesdev')
+    CEPH_SALT_REPO = 'https://github.com/ceph/ceph-salt'
+    CEPH_SALT_BRANCH = 'master'
     CONFIG_FILE = os.path.join(A_WORKING_DIR, 'config.yaml')
     DEBUG = False
     SSH_KEY_NAME = 'sesdev'  # do NOT use 'id_rsa'
@@ -411,7 +413,7 @@ SETTINGS = {
     'ceph_salt_git_branch': {
         'type': str,
         'help': 'ceph-salt git branch to use',
-        'default': 'master',
+        'default': None,
     },
     'stop_before_ceph_salt_config': {
         'type': bool,

sesdev/__init__.py
@@ -520,6 +520,22 @@ def _gen_settings_dict(version,
         settings_dict['encrypted_osds'] = encrypted_osds
 
     if ceph_salt_repo:
+        if not ceph_salt_branch:
+            logger.debug(
+                "User explicitly specified only --ceph-salt-repo; assuming --ceph-salt-branch %s",
+                seslib.GlobalSettings.CEPH_SALT_BRANCH
+                )
+            ceph_salt_branch = seslib.GlobalSettings.CEPH_SALT_BRANCH
+
+    if ceph_salt_branch:
+        if not ceph_salt_repo:
+            logger.debug(
+                "User explicitly specified only --ceph-salt-branch; assuming --ceph-salt-repo %s",
+                seslib.GlobalSettings.CEPH_SALT_REPO
+                )
+            ceph_salt_repo = seslib.GlobalSettings.CEPH_SALT_REPO
+
+    if ceph_salt_repo:
         settings_dict['ceph_salt_git_repo'] = ceph_salt_repo
 
     if ceph_salt_branch:

seslib/templates/ceph-salt/ceph_salt_deployment.sh.j2
@@ -1,7 +1,7 @@
 
 set -ex
 
-{% if ceph_salt_git_repo %}
+{% if ceph_salt_git_repo and ceph_salt_git_branch %}
 # install ceph-salt
 cd /root
 git clone {{ ceph_salt_git_repo }}

commit 6bd81c80a3ff5f3f69332b33b7b64d6dd7fd57b4

seslib: ceph_salt_git_branch might be None
231e50d41c62a8c62485fd59b3ca79503cd264c2 changed the default value of
this setting to None, which caused a regression.

Fixes: 231e50d41c62a8c62485fd59b3ca79503cd264c2
Fixes: https://github.com/SUSE/sesdev/issues/216
Signed-off-by: Nathan Cutler <ncutler@suse.com>

seslib/__init__.py
@@ -1079,15 +1079,16 @@ class Deployment():
         # detect whether we need to fetch github PRs
         ceph_salt_fetch_github_pr_heads = False
         ceph_salt_fetch_github_pr_merges = False
-        if self.settings.ceph_salt_git_branch.startswith('origin/pr/'):
+        ceph_salt_git_branch = self.settings.ceph_salt_git_branch
+        if ceph_salt_git_branch and ceph_salt_git_branch.startswith('origin/pr/'):
             log_msg = ("Detected special ceph-salt GitHub PR (HEAD) branch {}"
-                       .format(self.settings.ceph_salt_git_branch)
+                       .format(ceph_salt_git_branch)
                        )
             logger.info(log_msg)
             ceph_salt_fetch_github_pr_heads = True
-        elif self.settings.ceph_salt_git_branch.startswith('origin/pr-merged/'):
+        elif ceph_salt_git_branch and ceph_salt_git_branch.startswith('origin/pr-merged/'):
             log_msg = ("Detected special ceph-salt GitHub PR (MERGE) branch {}"
-                       .format(self.settings.ceph_salt_git_branch)
+                       .format(ceph_salt_git_branch)
                        )
             logger.info(log_msg)
             ceph_salt_fetch_github_pr_merges = True