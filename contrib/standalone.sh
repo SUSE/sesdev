d006f2cf8b530554a8a9abbfbcede927a4d4deb6
Nathan Cutler <ncutler@suse.com>
2020-05-19 17:41:52 +0200

    contrib/standalone.sh: add coverage for "supportconfig"
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- contrib/standalone.sh
+++ contrib/standalone.sh
@@ -182,6 +182,7 @@ if [ "$SES5" ] ; then
     run_cmd sesdev destroy --non-interactive ses5-1node
     run_cmd sesdev create ses5 --non-interactive --roles "[master,client,openattic],[storage,mon,mgr,rgw],[storage,mon,mgr,mds,nfs],[storage,mon,mgr,mds,rgw,nfs]" ses5-4node
     run_cmd sesdev qa-test ses5-4node
+    run_cmd sesdev supportconfig ses5-4node node1
     # consider uncommenting after the following bugs are fixed:
     # - https://github.com/SUSE/sesdev/issues/276
     # - https://github.com/SUSE/sesdev/issues/291
@@ -205,6 +206,7 @@ if [ "$SES6" ] ; then
     run_cmd sesdev destroy --non-interactive ses6-1node
     run_cmd sesdev create ses6 --non-interactive ses6-4node
     run_cmd sesdev qa-test ses6-4node
+    run_cmd sesdev supportconfig ses6-4node node1
     # consider uncommenting after the following bugs are fixed:
     # - https://github.com/SUSE/sesdev/issues/276
     # - https://github.com/SUSE/sesdev/issues/291
@@ -228,6 +230,7 @@ if [ "$SES7" ] ; then
     run_cmd sesdev destroy --non-interactive ses7-1node
     run_cmd sesdev create ses7 --non-interactive $CEPH_SALT_FROM_SOURCE ses7-4node
     run_cmd sesdev qa-test ses7-4node
+    run_cmd sesdev supportconfig ses7-4node node1
     # consider uncommenting after the following bugs are fixed:
     # - bsc#1170498
     # - https://github.com/SUSE/sesdev/issues/276
