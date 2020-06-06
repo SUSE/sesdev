cf327719fcc5d76ea0d54db0a99342b6c1dfdfd3
Nathan Cutler <ncutler@suse.com>
2020-06-06 13:18:29 +0200

    Deployment: vet deployment ID before creation
    It's not a good idea to allow users to specify a deployment ID that
    would not be a valid hostname element [1], since the deployment ID
    might be used as a hostname element.
    
    [1] from hostname(7) - Linux manual page:
    
    Each element of the hostname must be from 1 to 63 characters long and the
    entire hostname, including the dots, can be at most 253 characters long.
    Valid characters for hostnames are ASCII(7) letters from a to z, the digits
    from 0 to 9, and the hyphen (-).
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

---
+++ tests/test_vet_dep_id.py
@@ -0,0 +1,25 @@
+import pytest
+
+from seslib import _vet_dep_id
+from seslib.exceptions import DepIDWrongLength, DepIDIllegalChars
+
+def test_vet_dep_id():
+    assert _vet_dep_id("hooholopar") == "hooholopar"
+    assert _vet_dep_id("oron1anio0") == "oron1anio0"
+    assert _vet_dep_id("hyphen-words-separated") == "hyphen-words-separated"
+    with pytest.raises(DepIDWrongLength):
+        _vet_dep_id("")
+    with pytest.raises(DepIDWrongLength):
+        long_string = (
+            "hooholoparhooholoparhooholoparhooholoparhooh"
+            "hooholoparhooholoparhooholoparhooholoparhooh"
+            "hooholoparhooholoparhooholoparhooholoparhooh"
+            "hooholoparhooholoparhooholoparhooholoparhooh"
+            )
+        _vet_dep_id(long_string)
+    with pytest.raises(DepIDIllegalChars):
+        _vet_dep_id("hooholopar;")
+    with pytest.raises(DepIDIllegalChars):
+        _vet_dep_id("master]")
+    with pytest.raises(DepIDIllegalChars):
+        _vet_dep_id("master_id")
