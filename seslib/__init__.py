SUSE/sesdev

commit f67dbb87cfba4c9a10ff0f02720f325ede994a6e

seslib: no longer support nautilus deployment in Tumbleweed
Since Tumbleweed is now on Octopus, nautilus is not expected to be built
for Factory/Tumbleweed anymore in filesystems:ceph:nautilus.

Also, the "octopus" deployment target is expected to deploy something
very similar, if not identical, to SES7 on openSUSE Leap 15.2. So use
filesystems:ceph:octopus by default, instead of
filesystems:ceph:octopus:upstream.

Signed-off-by: Nathan Cutler <ncutler@suse.com>

seslib/__init__.py
@@ -238,10 +238,6 @@ VERSION_OS_REPO_MAPPING = {
             'https://download.opensuse.org/repositories/filesystems:/ceph:/nautilus/'
             'openSUSE_Leap_15.1/'
         ],
-        'tumbleweed': [
-            'https://download.opensuse.org/repositories/filesystems:/ceph:/nautilus/'
-            'openSUSE_Tumbleweed'
-        ],
     },
     'ses6': {
         'sles-15-sp1': [
@@ -255,7 +251,7 @@ VERSION_OS_REPO_MAPPING = {
             'openSUSE_Leap_15.1'
         ],
         'leap-15.2': [
-            'https://download.opensuse.org/repositories/filesystems:/ceph:/octopus:/upstream/'
+            'https://download.opensuse.org/repositories/filesystems:/ceph:/octopus/'
             'openSUSE_Leap_15.2'
         ],
         'tumbleweed': [

commit e9aecd884097fc4abf62cebee247490c118b0bab

seslib: do not use CR container for octopus deployment
By default, we were using the CR RPMs/containers for the octopus
deployment, but that's no longer appropriate because we want users to
get the same code when they deploy "octopus" as when they deploy "ses7".

f67dbb87cfba4c9a10ff0f02720f325ede994a6e fixed this for the RPMs, but
neglected to make the same change for the container.

Fixes: f67dbb87cfba4c9a10ff0f02720f325ede994a6e
Signed-off-by: Nathan Cutler <ncutler@suse.com>

seslib/__init__.py
@@ -172,7 +172,7 @@ OS_REPOS = {
 
 IMAGE_PATHS = {
     'ses7': 'registry.suse.de/devel/storage/7.0/containers/ses/7/ceph/ceph',
-    'octopus': 'registry.opensuse.org/filesystems/ceph/octopus/upstream/images/ceph/ceph',
+    'octopus': 'registry.opensuse.org/filesystems/ceph/octopus/images/ceph/ceph',
     'pacific': 'registry.opensuse.org/filesystems/ceph/master/upstream/images/ceph/ceph',
 }
 