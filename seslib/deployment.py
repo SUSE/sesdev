SUSE/sesdev

commit 6d18054d774faf8d4aea3802b4bfe666c9189e91

deepsea: add ganesha roles to policy.cfg
One fine day, we noticed that sesdev was not adding ganesha roles to
policy.cfg in {ses5,nautilus,ses6} clusters.

Signed-off-by: Nathan Cutler <ncutler@suse.com>

seslib/templates/salt/deepsea/nautilus_policy.cfg.j2
@@ -7,41 +7,34 @@ role-admin/cluster/*.sls
 {% if node.has_role('storage') %}
 role-storage/cluster/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('mon') %}
 role-mon/cluster/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('master') %}
 role-master/cluster/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('mgr') %}
 role-mgr/cluster/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('mds') %}
 role-mds/cluster/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('rgw') %}
 role-rgw/cluster/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('igw') %}
 role-igw/cluster/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('nfs') %}
 role-ganesha/cluster/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('prometheus') %}
 role-prometheus/cluster/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('grafana') %}
 role-grafana/cluster/{{ node.name }}*.sls
 {% endif %}
-
+{% if node.has_role('ganesha') %}
+role-ganesha/cluster/{{ node.name }}*.sls
+{% endif %}
 {% endfor %}

seslib/templates/salt/deepsea/ses5_policy.cfg.j2
@@ -7,39 +7,32 @@ profile-default/cluster/*.sls
 profile-default/stack/default/ceph/minions/*yml
 
 {% for node in nodes: %}
-
 {% if node.has_role('mon') %}
 role-mon/cluster/{{ node.name }}*.sls
 role-mon/stack/default/ceph/minions/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('master') %}
 role-master/cluster/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('mgr') %}
 role-mgr/cluster/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('mds') %}
 role-mds/cluster/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('rgw') %}
 role-rgw/cluster/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('igw') %}
 role-igw/cluster/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('nfs') %}
 role-ganesha/cluster/{{ node.name }}*.sls
 {% endif %}
-
 {% if node.has_role('openattic') %}
 role-openattic/cluster/{{ node.name }}*.sls
 {% endif %}
-
-
+{% if node.has_role('ganesha') %}
+role-ganesha/cluster/{{ node.name }}*.sls
+{% endif %}
 {% endfor %}

commit 62e199f348c2d7daa002048c896bb394990f46b7

deepsea: properly recognize deprecated ganesha role
As far as sesdev is concerned, the role "ganesha" should not be used,
because it has been renamed to "nfs". However, this can be confusing.
First, we had to keep the "ganesha" role around to avoid breaking
running clusters. Second, DeepSea also has a concept of "roles" and, as
far as DeepSea is concerned, the role is still called "ganesha".

This commit tries to bring some order to the chaos by:

1. tolerating "ganesha" (as a synonym for "nfs") in clusters
   that use DeepSea as a deployment tool
2. raising an exception, with the effect of displaying a helpful message,
   if someone unknowingly specifies a "ganesha" role for a post-nautilus
   cluster.

Fixes: 6d18054d774faf8d4aea3802b4bfe666c9189e91
Signed-off-by: Nathan Cutler <ncutler@suse.com>

seslib/deployment.py
@@ -22,6 +22,7 @@ from .exceptions import \
                         ExplicitAdminRoleNotAllowed, \
                         MultipleRolesPerMachineNotAllowedInCaaSP, \
                         NodeDoesNotExist, \
+                        NoGaneshaRolePostNautilus, \
                         NoPrometheusGrafanaInSES5, \
                         NoSourcePortForPortForwarding, \
                         NoStorageRolesDeepsea, \
@@ -852,6 +853,10 @@ deployment might not be completely destroyed.
                     raise NoStorageRolesCephadm('nfs')
                 if self.node_counts['mds'] > 0:
                     raise NoStorageRolesCephadm('mds')
+        # ganesha role only allowed pre-octopus
+        if self.settings.version in ['octopus', 'ses7', 'pacific']:
+            if self.node_counts["ganesha"] > 0:
+                raise NoGaneshaRolePostNautilus()
         # there must not be more than one suma role:
         if self.node_counts['suma'] > 1:
             raise UniqueRoleViolation('suma', self.node_counts['suma'])

seslib/exceptions.py
@@ -102,6 +102,15 @@ class NodeDoesNotExist(SesDevException):
             "Node '{}' does not exist in this deployment".format(node))
 
 
+class NoGaneshaRolePostNautilus(SesDevException):
+    def __init__(self):
+        super(NoGaneshaRolePostNautilus, self).__init__(
+            "You specified a \"ganesha\" role. In cephadm, NFS-Ganesha daemons "
+            "are referred to as \"nfs\" daemons, so in sesdev the role has been "
+            "renamed to \"nfs\". Please change all instances of \"ganesha\" to "
+            "\"nfs\" in your roles string and try again")
+
+
 class NoExplicitRolesWithSingleNode(SesDevException):
     def __init__(self):
         super(NoExplicitRolesWithSingleNode, self).__init__(

seslib/templates/salt/deepsea/nautilus_policy.cfg.j2
@@ -25,7 +25,7 @@ role-rgw/cluster/{{ node.name }}*.sls
 {% if node.has_role('igw') %}
 role-igw/cluster/{{ node.name }}*.sls
 {% endif %}
-{% if node.has_role('nfs') %}
+{% if node.has_role('nfs') or node.has_role('ganesha') %}
 role-ganesha/cluster/{{ node.name }}*.sls
 {% endif %}
 {% if node.has_role('prometheus') %}
@@ -33,8 +33,5 @@ role-prometheus/cluster/{{ node.name }}*.sls
 {% endif %}
 {% if node.has_role('grafana') %}
 role-grafana/cluster/{{ node.name }}*.sls
-{% endif %}
-{% if node.has_role('ganesha') %}
-role-ganesha/cluster/{{ node.name }}*.sls
 {% endif %}
 {% endfor %}

seslib/templates/salt/deepsea/ses5_policy.cfg.j2
@@ -26,13 +26,10 @@ role-rgw/cluster/{{ node.name }}*.sls
 {% if node.has_role('igw') %}
 role-igw/cluster/{{ node.name }}*.sls
 {% endif %}
-{% if node.has_role('nfs') %}
+{% if node.has_role('nfs') or node.has_role('ganesha') %}
 role-ganesha/cluster/{{ node.name }}*.sls
 {% endif %}
 {% if node.has_role('openattic') %}
 role-openattic/cluster/{{ node.name }}*.sls
-{% endif %}
-{% if node.has_role('ganesha') %}
-role-ganesha/cluster/{{ node.name }}*.sls
 {% endif %}
 {% endfor %}