SUSE/sesdev

commit b939f1550d00293f75efec78a6a8492aad38ca45

constant: shorten long lines that caused pylint warnings
I inadvertently merged a commit that contained some lines that exceeded
the 100 character limit.

Fixes: f9804624401854d722b477d792f664a115f2afbb
Signed-off-by: Nathan Cutler <ncutler@suse.com>

seslib/constant.py
@@ -165,7 +165,8 @@ class Constant():
             'server-apps-update': 'http://download.nue.suse.com/ibs/SUSE/Updates/'
                                   'SLE-Module-Server-Applications/15-SP1/x86_64/update/',
             'storage': 'http://download.nue.suse.com/ibs/SUSE/Products/Storage/6/x86_64/product/',
-            'storage-update': 'http://download.nue.suse.com/ibs/SUSE/Updates/Storage/6/x86_64/update/'
+            'storage-update': 'http://download.nue.suse.com/ibs/SUSE/Updates/Storage/6/x86_64/',
+                              'update/'
         },
         'sles-15-sp2': {
             'product': 'http://dist.suse.de/ibs/SUSE/Products/SLE-Product-SLES/15-SP2/x86_64/'
@@ -180,8 +181,9 @@ class Constant():
                            'SLE-Module-Server-Applications/15-SP2/x86_64/product/',
             'server-apps-update': 'http://download.nue.suse.com/ibs/SUSE/Updates/'
                                   'SLE-Module-Server-Applications/15-SP2/x86_64/update/',
-            'storage7-media': 'http://download.nue.suse.com/ibs/SUSE:/SLE-15-SP2:/Update:/Products:/'
-                              'SES7/images/repo/SUSE-Enterprise-Storage-7-POOL-x86_64-Media1/',
+            'storage7-media': 'http://download.nue.suse.com/ibs/SUSE:/SLE-15-SP2:/Update:/'
+                              'Products:/SES7/images/repo/'
+                              'SUSE-Enterprise-Storage-7-POOL-x86_64-Media1/',
         },
     }
 
@@ -307,16 +309,16 @@ class Constant():
         },
         'caasp4': {
             'sles-15-sp2': [
-                'http://download.nue.suse.com/ibs/SUSE/Products/SLE-Module-Containers/15-SP2/x86_64/'
-                'product/',
+                'http://download.nue.suse.com/ibs/SUSE/Products/SLE-Module-Containers/15-SP2/'
+                'x86_64/product/',
                 'http://download.nue.suse.com/ibs/SUSE/Updates/SLE-Module-Containers/15-SP2/x86_64/'
                 'update/',
-                'http://download.nue.suse.com/ibs/SUSE/Products/SLE-Module-Public-Cloud/15-SP2/x86_64/'
-                'product/',
-                'http://download.nue.suse.com/ibs/SUSE/Updates/SLE-Module-Public-Cloud/15-SP2/x86_64/'
-                'update/',
-                'http://download.nue.suse.com/ibs/SUSE/Updates/SLE-Module-Public-Cloud/15-SP1/x86_64/'
-                'update/',
+                'http://download.nue.suse.com/ibs/SUSE/Products/SLE-Module-Public-Cloud/15-SP2/'
+                'x86_64/product/',
+                'http://download.nue.suse.com/ibs/SUSE/Updates/SLE-Module-Public-Cloud/15-SP2/'
+                'x86_64/update/',
+                'http://download.nue.suse.com/ibs/SUSE/Updates/SLE-Module-Public-Cloud/15-SP1/'
+                'x86_64/update/',
                 'http://download.nue.suse.com/ibs/SUSE:/SLE-15-SP2:/Update:/Products:/CaaSP:/4.5/'
                 'standard/',
                 'http://download.nue.suse.com/ibs/SUSE/Updates/SUSE-CAASP/4.5/x86_64/update/'

commit 9665e101041d9bc4e5b7b9074ecccc8374fdb857

seslib/constant.py: fix syntax error introduced carelessly
I carelessly pushed a commit to fix some long lines without noticing that it
also introduced a syntax error!

Fixes: b939f1550d00293f75efec78a6a8492aad38ca45
Signed-off-by: Nathan Cutler <ncutler@suse.com>

seslib/constant.py
@@ -165,8 +165,8 @@ class Constant():
             'server-apps-update': 'http://download.nue.suse.com/ibs/SUSE/Updates/'
                                   'SLE-Module-Server-Applications/15-SP1/x86_64/update/',
             'storage': 'http://download.nue.suse.com/ibs/SUSE/Products/Storage/6/x86_64/product/',
-            'storage-update': 'http://download.nue.suse.com/ibs/SUSE/Updates/Storage/6/x86_64/',
-                              'update/'
+            'storage-update': 'http://download.nue.suse.com/ibs/SUSE/Updates/Storage/6/x86_64/'
+                              'update/',
         },
         'sles-15-sp2': {
             'product': 'http://dist.suse.de/ibs/SUSE/Products/SLE-Product-SLES/15-SP2/x86_64/'