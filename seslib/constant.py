SUSE/sesdev

commit 56d03a59688abe5d4c89e81e0899b8aa2b676694

constant.py: drop nfs from octopus default roles
Attempt to workaround:
Bug 1183740 - ceph octopus 15.2.9 fails to deploy rgw and nfs

Works-around: https://bugzilla.opensuse.org/show_bug.cgi?id=1183740
Signed-off-by: Nathan Cutler <ncutler@suse.com>

seslib/constant.py
@@ -237,7 +237,12 @@ class Constant():
         "octopus": [["admin", "master", "client", "prometheus", "grafana", "alertmanager",
                      "node-exporter"],
                     ["bootstrap", "storage", "mon", "mgr", "rgw", "igw", "node-exporter"],
-                    ["storage", "mon", "mgr", "mds", "igw", "nfs", "node-exporter"],
+                    ["storage", "mon", "mgr", "mds", "igw", "node-exporter"],
+                    ["storage", "mon", "mgr", "mds", "rgw", "node-exporter"]],
+        "ses7": [["admin", "master", "client", "prometheus", "grafana", "alertmanager",
+                     "node-exporter"],
+                    ["bootstrap", "storage", "mon", "mgr", "rgw", "igw", "node-exporter"],
+                    ["storage", "mon", "mgr", "mds", "igw", "node-exporter"],
                     ["storage", "mon", "mgr", "mds", "rgw", "nfs", "node-exporter"]]
     }
 
@@ -249,7 +254,7 @@ class Constant():
         'pacific': ROLES_DEFAULT["octopus"],
         'ses5': ROLES_DEFAULT["luminous"],
         'ses6': ROLES_DEFAULT["nautilus"],
-        'ses7': ROLES_DEFAULT["octopus"],
+        'ses7': ROLES_DEFAULT["ses7"],
     }
 
     ROLES_KNOWN = [
@@ -282,7 +287,9 @@ class Constant():
         "nautilus": "[ master, storage, mon, mgr, prometheus, grafana, mds, igw, rgw, "
                     "nfs ]",
         "octopus": "[ master, admin, bootstrap, storage, mon, mgr, mds, igw, rgw, "
-                   "nfs, prometheus, grafana, alertmanager, node-exporter ]",
+                   "prometheus, grafana, alertmanager, node-exporter ]",
+        "ses7": "[ master, admin, bootstrap, storage, mon, mgr, mds, igw, rgw, "
+                   "prometheus, grafana, alertmanager, node-exporter ]",
     }
 
     SSH_KEY_NAME = 'sesdev'  # do NOT use 'id_rsa'

sesdev/__init__.py
@@ -412,7 +412,9 @@ def _gen_settings_dict(
         raise NoExplicitRolesWithSingleNode()
     elif single_node:
         roles_string = ""
-        if version in ['ses7', 'octopus', 'pacific']:
+        if version in ['ses7']:
+            roles_string = Constant.ROLES_SINGLE_NODE['ses7']
+        elif version in ['octopus', 'pacific']:
             roles_string = Constant.ROLES_SINGLE_NODE['octopus']
         elif version in ['ses6', 'nautilus']:
             roles_string = Constant.ROLES_SINGLE_NODE['nautilus']

commit 9092315b13b2ae28f16eb73b5af1426b96444933

constant.py: fix pycodestyle regression
In my haste, I introduced a pycodestyle regression. This commit fixes it.

Fixes: 56d03a59688abe5d4c89e81e0899b8aa2b676694
Signed-off-by: Nathan Cutler <ncutler@suse.com>

seslib/constant.py
@@ -242,10 +242,10 @@ class Constant():
                     ["storage", "mon", "mgr", "mds", "igw", "node-exporter"],
                     ["storage", "mon", "mgr", "mds", "rgw", "node-exporter"]],
         "ses7": [["admin", "master", "client", "prometheus", "grafana", "alertmanager",
-                     "node-exporter"],
-                    ["bootstrap", "storage", "mon", "mgr", "rgw", "igw", "node-exporter"],
-                    ["storage", "mon", "mgr", "mds", "igw", "node-exporter"],
-                    ["storage", "mon", "mgr", "mds", "rgw", "nfs", "node-exporter"]]
+                  "node-exporter"],
+                 ["bootstrap", "storage", "mon", "mgr", "rgw", "igw", "node-exporter"],
+                 ["storage", "mon", "mgr", "mds", "igw", "node-exporter"],
+                 ["storage", "mon", "mgr", "mds", "rgw", "nfs", "node-exporter"]]
     }
 
     ROLES_DEFAULT_BY_VERSION = {