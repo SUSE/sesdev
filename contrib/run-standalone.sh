72dcab5d470b0d9fcee8ba66093f8ac9a05ddf05
Nathan Cutler <ncutler@suse.com>
2021-11-09 11:01:42 +0100

    contrib/standalone.sh: enable pacific testing in normal operation
    Also fail with a useful error message if someone specifies, e.g., "pacific" on
    the command line instead of "--pacific".
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- contrib/run-standalone.sh
+++ contrib/run-standalone.sh
@@ -3,7 +3,7 @@
 # run sesdev test script "contrib/standalone.sh" while leveraging
 # GNU script to capture all output in a file called "typescript"
 #
-SCRIPTNAME=$(basename ${0})
-BASEDIR=$(readlink -f "$(dirname ${0})")
-COMMAND="$BASEDIR/standalone.sh $@ ; exit"
+SCRIPTNAME="$(basename "$0")"
+BASEDIR="$(readlink -f "$(dirname "$0")")"
+COMMAND="$BASEDIR/standalone.sh $* ; exit"
 script -c "$COMMAND"
